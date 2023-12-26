3196e31c567f18fcc6dec45a9e862b55bd252ca4
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2023-12-26 11:06:03 -0300

    Fornecedores crud

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import winston from "winston"
 import ClientRouter from "./routes/client.router.js"
+import SupplierRouter from "./routes/supplier.router.js"
 
 const app = express()
 
@@ -27,6 +28,7 @@ global.logger = winston.createLogger({
 app.use(express.json())
 app.use(cors())
 app.use("/client", ClientRouter)
+app.use("/supplier", SupplierRouter)
 app.use((error, request, response, _next) => {
   logger.error(`${request.method} ${request.baseUrl} - ${error.message}`);
   response.status(400).send({
